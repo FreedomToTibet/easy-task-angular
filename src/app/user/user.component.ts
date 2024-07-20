29c59b0e522b89bc22df4532443f7b064e724192
FreedomToTibet <98056258+FreedomToTibet@users.noreply.github.com>
2024-07-20 00:40:20 -0700

    using "signal"

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+
+const randomUser = DUMMY_USERS[Math.floor(Math.random() * DUMMY_USERS.length)];
 
 @Component({
   selector: 'app-user',
@@ -7,5 +10,13 @@ import { Component } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
+	selectedUser = signal(randomUser);
 
+	get imagePath() {
+		return `/images/${this.selectedUser().avatar}`;
+	}
+
+	onSelectedUser() {
+		this.selectedUser.set(DUMMY_USERS[Math.floor(Math.random() * DUMMY_USERS.length)]);
+	}
 }
