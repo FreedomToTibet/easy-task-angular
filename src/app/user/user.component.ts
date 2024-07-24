4a9066698e95b52f8a244df30480999a7994abfa
FreedomToTibet <98056258+FreedomToTibet@users.noreply.github.com>
2024-07-24 00:45:47 -0700

    app component typing

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -7,16 +7,21 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-	@Input({required: true}) id!: string;
-	@Input({required: true}) avatar!: string;
-	@Input({required: true}) name!: string;
-	@Output() select = new EventEmitter();
+	@Input({required: true}) user!: {
+		id: string;
+		avatar: string;
+		name: string;
+	}
+	// @Input({required: true}) id!: string;
+	// @Input({required: true}) avatar!: string;
+	// @Input({required: true}) name!: string;
+	@Output() select = new EventEmitter<string>();
 
 	get imagePath() {
-		return `images/${this.avatar}`;
+		return `images/${this.user.avatar}`;
 	}
 
 	onSelectedUser() {
-		this.select.emit(this.id);
+		this.select.emit(this.user.id);
 	}
 }
