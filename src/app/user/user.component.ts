61c422cb268867d97967f0eeae7e628a0c6ff639
FreedomToTibet <98056258+FreedomToTibet@users.noreply.github.com>
2024-07-24 00:52:45 -0700

    interface User added

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,17 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 
+// type User = {
+// 	id: string;
+// 	name: string;
+// 	avatar: string;
+// };
+
+interface User {
+	id: string;
+	name: string;
+	avatar: string;
+};
+
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -7,14 +19,7 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-	@Input({required: true}) user!: {
-		id: string;
-		avatar: string;
-		name: string;
-	}
-	// @Input({required: true}) id!: string;
-	// @Input({required: true}) avatar!: string;
-	// @Input({required: true}) name!: string;
+	@Input() user!: User;
 	@Output() select = new EventEmitter<string>();
 
 	get imagePath() {
